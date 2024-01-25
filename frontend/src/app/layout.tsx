ac478845046efb1c9065a271c797bb4e9a44c5f4
Md Rashed Ali <tools.rashed@gmail.com>
2024-01-25 18:18:59 +0600

    add /footer

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,41 +1,45 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import DesktopNav from '@/components/ui/navigation/DesktopNav'
-import MobileNav from '@/components/ui/navigation/MobileNav'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import DesktopNav from "@/components/ui/navigation/DesktopNav";
+import MobileNav from "@/components/ui/navigation/MobileNav";
+import FooterComponent from "@/components/footer/FooterComponent";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ZossGadget',
-  description: 'One of the best E-commerce website in bangladesh',
-}
+  title: "ZossGadget",
+  description: "One of the best E-commerce website in bangladesh",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <div>
-           {/* MOBILE NAV */}
-      <div className="fixed border lg:hidden bottom-0 w-full overflow-hidden border-border">
-        <MobileNav />
-      </div>
-      {/* DESKTOP NAV */}
-      <div className="hidden lg:block">
-        <div className="p-3 rounded">
-          <DesktopNav />
+          {/* MOBILE NAV */}
+          <div className="fixed border lg:hidden bottom-0 w-full overflow-hidden border-border">
+            <MobileNav />
+          </div>
+          {/* DESKTOP NAV */}
+          <div className="hidden lg:block">
+            <div className="p-3 rounded">
+              <DesktopNav />
+            </div>
+          </div>
         </div>
-      </div>
-        </div>
+        {/* MAIN APP */}
+        <div>{children}</div>
+
+        {/* FOOTER */}
         <div>
-        {children}
+          <FooterComponent/>
         </div>
-       
-        </body>
+      </body>
     </html>
-  )
+  );
 }
---
+++ frontend/src/components/footer/FooterComponent.tsx
@@ -0,0 +1,135 @@
+import Link from "next/link";
+import {
+  FaSquareFacebook,
+  FaSquareInstagram,
+  FaSquareWhatsapp,
+  FaSquareYoutube,
+  FaSquarePhone, 
+  FaLocationDot,
+  FaMapLocationDot
+} from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+const FooterComponent = () => {
+  return (
+    <div className="bg-muted">
+      <div className="flex items-end w-full">
+        <footer className="w-full text-muted-foreground">
+          {/* FOOTER CONTENT START */}
+          <div className="max-w-[1240px] mx-auto">
+            {/* LOGO INFO */}
+            <div className="">
+              <a className="flex items-center justify-center font-medium text-black title-font md:justify-start">
+                ZossGadget
+              </a>
+              <p className="mt-2 text-sm ">Elevating Your Tech Experience</p>
+              {/* SOCIAL ICONS */}
+              <div className="mt-4 flex gap-4">
+                <FaSquareFacebook />
+                <FaSquareInstagram />
+                <FaSquareYoutube />
+              </div>
+            </div>
+
+            {/* ABOUT US */}
+            <div className="">
+              <h2 className="mb-3 text-sm font-medium tracking-widest text-black uppercase title-font">
+                About
+              </h2>
+              <nav className="mb-10 list-none">
+                <li className="mt-3">
+                  <a className=" cursor-pointer hover:text-black">Company</a>
+                </li>
+                <li className="mt-3">
+                  <a className=" cursor-pointer hover:text-black">Careers</a>
+                </li>
+                <li className="mt-3">
+                  <a className=" cursor-pointer hover:text-black">Blog</a>
+                </li>
+              </nav>
+            </div>
+
+            {/* INFORMATION */}
+            <div className="">
+              <h2 className="mb-3 text-sm font-medium tracking-widest text-black uppercase title-font">
+                Support
+              </h2>
+              <nav className="mb-10 list-none">
+                <li className="mt-3">
+                  <a className=" cursor-pointer hover:text-black">
+                    Contact Support
+                  </a>
+                </li>
+                <li className="mt-3">
+                  <a className=" cursor-pointer hover:text-black">
+                    Help Resources
+                  </a>
+                </li>
+                <li className="mt-3">
+                  <a className=" cursor-pointer hover:text-black">
+                    Release Updates
+                  </a>
+                </li>
+              </nav>
+            </div>
+
+            {/* CONTACT */}
+            <div className="">
+              <h2 className="mb-3 text-sm font-medium tracking-widest text-black uppercase title-font">
+                Contact
+              </h2>
+              <nav className="mb-10 list-none">
+                <li className="mt-3 flex gap-2 items-center">
+                  <FaSquareWhatsapp className="text-xl" />
+                  <Link
+                    href="https://api.whatsapp.com/send?phone=8801318044400"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className=" cursor-pointer text-blue-500 hover:underline "
+                  >
+                    Send a Message
+                  </Link>
+                </li>
+                <li className="mt-3 cursor-pointer hover:text-black flex items-center gap-2">
+                  <MdEmail className="text-xl" />
+                  <p>support@zossgadget.com.bd</p>
+                </li>
+                <li className="mt-3 cursor-pointer hover:text-black flex items-center gap-2">
+                  <FaSquarePhone  className="text-xl" />
+                  <p> +8801318-944400</p>
+                </li>
+                <li className="mt-3 cursor-pointer hover:text-black flex items-start gap-2">
+                  <FaLocationDot className="text-xl" />
+                  <p>
+                    Haldibari Hat, Baliadangi, <br /> Thakuragaon. Bangladesh
+                  </p>
+                </li>
+                <li className="mt-1 cursor-pointer  flex items-center gap-2">
+                <FaMapLocationDot className="text-xl"/>
+                  <Link
+                    href="https://maps.app.goo.gl/Pc9c16Z2zgRtJScY6"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-blue-500 hover:underline"
+                  >
+                    See on Map
+                  </Link>
+                </li>
+              </nav>
+            </div>
+          </div>
+
+          {/* COPYRIGHT */}
+          <div className="bg-foreground text-white">
+            <div className="max-w-[1240px] mx-auto py-4">
+              <p className="text-sm capitalize lg:text-center">
+                &copy; 2024 ZossGadget All rights reserved
+              </p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default FooterComponent;
