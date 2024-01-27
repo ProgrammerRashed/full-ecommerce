9e88d190c3a66065728f9c98ebfa9bdc0f090733
Md Rashed Ali <tools.rashed@gmail.com>
2024-01-27 12:32:45 +0600

    improve /best selling products

--- client/src/components/best-selling/BestSellingProducts.tsx
+++ client/src/components/best-selling/BestSellingProducts.tsx
@@ -1,17 +1,24 @@
-import ProductCard from "../shared/ProductCard"
+import { PaginationComponent } from "../shared/PaginationComponent";
+import ProductCard from "../shared/ProductCard";
 
 const BestSellingProducts = () => {
   return (
     <div className="py-20">
-      <h1 className="text-2xl text-center mb-10 font-bold">Best Selling Products</h1>
+      <h1 className="text-2xl text-center mb-10 font-bold">
+        Best Selling Products
+      </h1>
 
-      <div className="best-selling-products grid grid-cols-3 gap-5">
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
+      <div className="best-selling-products grid grid-cols-4 gap-5">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+      <div className="mt-5">
+        <PaginationComponent />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BestSellingProducts
+export default BestSellingProducts;
