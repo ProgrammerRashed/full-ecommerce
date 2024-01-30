4c55e4b192966121b5a1348a17dc5f349a1a4910
Md Rashed Ali <tools.rashed@gmail.com>
2024-01-30 20:12:43 +0600

    add order page

--- client/src/app/profile/orders/page.tsx
+++ client/src/app/profile/orders/page.tsx
@@ -3,7 +3,17 @@ import React from 'react'
 const OrdersPage = () => {
   return (
     <div>
-      OrdersPage
+      <h1 className='text-xl font-bold mb-10'>My Orders</h1>
+      <div className='flex justify-between items-center'>
+        <div >
+          <p className='font-medium'>Order Number: ##################################</p>
+          <p className='text-sm'>Total: 200.00৳</p>
+          <p className='text-sm'>Ordered On: 11/12/2023</p>
+         </div>
+        <div>
+          <p>View Order</p>
+        </div>
+      </div>
     </div>
   )
 }
