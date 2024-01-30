4f6e5287676d006e728d8d50f0029736d190b265
Md Rashed Ali <tools.rashed@gmail.com>
2024-01-30 17:10:16 +0600

    improve /profile page

--- client/src/app/profile/layout.tsx
+++ client/src/app/profile/layout.tsx
@@ -1,6 +1,10 @@
+import ContinueShoppingCard from "@/components/cart/ContinueShoppingCard";
 import type { Metadata } from "next";
 import { headers } from "next/headers";
 import Link from "next/link";
+import { FaRegUser } from "react-icons/fa";
+import { FiShoppingBag } from "react-icons/fi";
+import { IoMdLogOut } from "react-icons/io";
 export const metadata: Metadata = {
   title: "ZossGadget | Profile",
   description: "One of the best E-commerce website in bangladesh",
@@ -18,12 +22,25 @@ export default function ProfileLayout({
   const targetPathname = pathName[pathName.length - 1];
 
   return (
-   <div className="grid grid-cols-12 gap-5">
-    <div className="col-span-3">
-        <Link href="/profile/orders">Orders</Link>
-        <Link href="/profile/personal-info">Info</Link>
+    <div className="max-w-[1240px] mx-auto">
+      <h1 className="text-4xl font-bold">My Profile</h1>
+
+      <div className="grid grid-cols-12 gap-5 mt-8 min-h-[60vh]">
+        <div className="col-span-3">
+         
+            <nav className="flex flex-col justify-start items-start text-lg gap-1">
+              <Link href="/profile/personal-info" className="flex gap-2 items-center p-2 hover:bg-muted/70 w-full"><FaRegUser/> Personal Information</Link>
+              <Link href="/profile/orders" className="flex gap-2 items-center p-2 hover:bg-muted/70 w-full"><FiShoppingBag/> Orders</Link>
+              <Link href="/profile/orders" className="flex gap-2 items-center p-2 hover:bg-muted/70 w-full"><IoMdLogOut/>Logout</Link>
+            </nav>
+        
+        </div>
+        <div className="col-span-9">{children}</div>
+      </div>
+
+      <div>
+        <ContinueShoppingCard/>
+      </div>
     </div>
-    <div className="col-span-9">{children}</div>
-   </div>
   );
 }
--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -3,7 +3,28 @@ import React from 'react'
 const MainProfilePage = () => {
   return (
     <div>
-      Main Profile
+
+    <div className='bg-accent p-5 space-y-4'>
+    <h1 className='text-xl'>Personal Profile</h1>
+    <div>
+      <p>Md Rashed Ali</p>
+      <p>shopping.rashed@gmail.com</p>
+    </div>
+    <p className='text-blue-400 hover:underline'>Subscribe to Newsletter</p>
+    </div>
+    <div className='bg-accent p-5 space-y-4'>
+    <h1 className='text-xl'>Personal Profile</h1>
+    <div>
+      <p>Md Rashed Ali</p>
+      <p>+8801738313337</p>
+      {/* ADDRESS */}
+      <div>
+        <p>Haldibari Hat</p>
+        <p>Rangpur - Thakurgaon - Baliadangi</p>
+      </div>
+    </div>
+    <p className='text-blue-400 hover:underline'>Subscribe to Newsletter</p>
+    </div>
     </div>
   )
 }
