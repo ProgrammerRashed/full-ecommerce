73eb0e37c2861850e7cb44eb3ff13180e7765b40
Md Rashed Ali <tools.rashed@gmail.com>
2024-02-03 13:06:13 +0600

    add /about page

---
+++ client/src/components/company-about/AboutComponent.tsx
@@ -0,0 +1,13 @@
+
+const AboutComponent = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-bold">About ZossGadget</h1>
+      <div className="w-full mt-5">
+        <p>At ZossGadget, we're driven by the fusion of brilliant minds and cutting-edge gadgets. Our goal is crystal clear: to enrich your tech journey by delivering meticulously crafted devices that seamlessly blend with your lifestyle. With a relentless pursuit of innovation and precision, each product reflects our dedication to enhancing your daily tech encounters. Explore our curated collection to discover a perfect harmony of functionality, style, and reliability, all crafted to uplift the quality of your life. ZossGadget, your premier eCommerce destination, is committed to bringing top-tier products to your fingertips.</p>
+      </div>
+    </div>
+  )
+}
+
+export default AboutComponent
