e823d264c28c09ef580aa4ede05c0527c09e2d9e
Md Rashed Ali <tools.rashed@gmail.com>
2024-09-23 12:51:52 +0600

    Added responsiveness to shop features

--- client/src/components/shop-features/ShopFeatures.tsx
+++ client/src/components/shop-features/ShopFeatures.tsx
@@ -5,23 +5,23 @@ import { FaCcMastercard } from "react-icons/fa";
 const ShopFeatures = () => {
   return (
     <div className="max-w-[1240px] mx-auto py-10 px-2 md:px-3 lg:px-5">
-      <div className="grid grid-cols-4 gap-5">
-        <div className="card space-y-2">
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-5">
+        <div className="card space-y-2 border rounded p-3">
           <FaShippingFast className="text-2xl" />
           <h1 className="font-bold">Free Shipping</h1>
           <p>Free shipping for order above ৳10,000</p>
         </div>
-        <div className="card space-y-2">
+        <div className="card space-y-2 border rounded p-3">
           <FaCcMastercard className="text-2xl" />
           <h1 className="font-bold">FLexible Payment</h1>
           <p>Pay with multiple payment method</p>
         </div>
-        <div className="card space-y-2">
+        <div className="card space-y-2 border rounded p-3">
           <BiSupport className="text-2xl" />
           <h1 className="font-bold">Online Support</h1>
           <p>24 hours a day, 7 days a week</p>
         </div>
-        <div className="card space-y-2">
+        <div className="card space-y-2 border rounded p-3">
           <TbTruckReturn className="text-2xl" />
           <h1 className="font-bold">Easy Return</h1>
           <p>Easily return damage product</p>
