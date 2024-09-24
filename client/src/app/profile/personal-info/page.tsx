2c8caae43304897382ff27f51d7ac0aab07512da
Md Rashed Ali <tools.rashed@gmail.com>
2024-09-24 11:23:09 +0600

    Updated profile page

--- client/src/app/profile/personal-info/page.tsx
+++ client/src/app/profile/personal-info/page.tsx
@@ -1,38 +1,57 @@
+"use client"
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Input } from '@/components/ui/input'
+import React, { useState } from 'react'
 
 const PersonalInfoPage = () => {
+  // State to manage whether fields are disabled or editable
+  const [isEditing, setIsEditing] = useState(false);
+
+  // Function to toggle editing mode
+  const handleEditClick = (event:any) => {
+    event.preventDefault(); // Prevents page reload
+    setIsEditing(!isEditing);
+  };
+
   return (
     <div>
-      <div className='info grid grid-cols-3 gap-5'>
+      <form 
+        className='info grid lg:grid-cols-3 gap-2 lg:gap-5' 
+        onSubmit={handleEditClick} // Handle form submission
+      >
         <div className='p-5'>
           <h1 className='text-sm text-muted-foreground mb-2'>Full Name</h1>
-          <p>Md Rashed Ali</p>
+          <Input name='name' type='text' disabled={!isEditing} value='Md Rashed Ali' />
         </div>
-         <div className='p-5'>
+        <div className='p-5'>
           <h1 className='text-sm text-muted-foreground mb-2'>Email Address</h1>
-          <p>shopping.rashed@gmail.com</p>
+          <Input name='email' type='email' disabled={!isEditing} value='shopping.rashed@gmail.com' />
         </div>
-         <div className='p-5'>
+        <div className='p-5'>
           <h1 className='text-sm text-muted-foreground mb-2'>Phone Number</h1>
-          <p>+8801738313337</p>
+          <Input name='phone' type='number' disabled={!isEditing} value='01738313337' />
         </div>
-
-         <div className='p-5'>
+        <div className='p-5'>
           <h1 className='text-sm text-muted-foreground mb-2'>Birthday</h1>
-          <p>25/01/2001</p>
+          <Input name='date-of-birth' type='date' disabled={!isEditing} value='2001-01-25' />
         </div>
-         <div className='p-5'>
+        <div className='p-5'>
           <h1 className='text-sm text-muted-foreground mb-2'>Gender</h1>
-          <p>Male</p>
+          <Input name='gender' type='text' disabled={!isEditing} value='Male' />
         </div>
-      </div>
+        <div className='p-5'>
+          <h1 className='text-sm text-muted-foreground mb-2'>Address Line 1</h1>
+          <Input name='address' type='text' disabled={!isEditing} value='Haldibari hat, Baliadangi, Thakurgaon' />
+        </div>
 
-      {/* ACTIONS */}
-      <div className='flex justify-start items-center gap-5 mt-4 p-5'>
-        <Button >Edit Profile</Button>
-        <Button variant="outline">Change Password</Button>
-      </div>
+        {/* ACTIONS */}
+        <div className='flex justify-start items-center gap-5 mt-4 p-5'>
+          <Button type='submit'>
+            {isEditing ? 'Save Profile' : 'Edit Profile'}
+          </Button>
+          <Button variant='outline' disabled={isEditing}>Change Password</Button>
+        </div>
+      </form>
     </div>
   )
 }
