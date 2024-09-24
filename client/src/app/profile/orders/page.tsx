cbb066baeb576690fc8238d7f23d4177766d6398
Md Rashed Ali <tools.rashed@gmail.com>
2024-09-24 11:00:06 +0600

    Updated order page

--- client/src/app/profile/orders/page.tsx
+++ client/src/app/profile/orders/page.tsx
@@ -1,19 +1,17 @@
+import { OrdersTable } from '@/components/profile/OrdersTable'
+import DataTable from '@/components/shared/DataTable'
 import React from 'react'
 
 const OrdersPage = () => {
   return (
     <div>
       <h1 className='text-xl font-bold mb-10'>My Orders</h1>
-      <div className='flex justify-between items-center'>
-        <div >
-          <p className='font-medium'>Order Number: ##################################</p>
-          <p className='text-sm'>Total: 200.00৳</p>
-          <p className='text-sm'>Ordered On: 11/12/2023</p>
-         </div>
+      
         <div>
-          <p>View Order</p>
+        <DataTable/>
         </div>
-      </div>
+
+   
     </div>
   )
 }
